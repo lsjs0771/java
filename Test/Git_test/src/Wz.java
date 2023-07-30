8ca781c56f7c40716f1532f70a15664997a9a759
lsjso771 <0771wangzhong@gmail.com>
2023-07-30 16:40:31 +0800

    no2

--- Test/Git_test/src/Wz.java
+++ Test/Git_test/src/Wz.java
@@ -1,5 +1,27 @@
-public class Wz {
-    public static void main(String[] args){
-        System.out.println("ok");
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Wz{
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String s="/home/wjs/12.png";
+        File file=new File(s);
+        String name=file.getName();
+
+        FileInputStream fileInputStream=new FileInputStream(file);
+        byte[] b=new byte[fileInputStream.available()];
+        fileInputStream.read(b);
+
+        ServerSocket serverSocket=new ServerSocket(3000);
+        Socket socket = serverSocket.accept();
+        OutputStream outputStream = socket.getOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeUTF(name);
+        objectOutputStream.flush();
+        objectOutputStream.writeObject(b);
+        objectOutputStream.flush();
+        socket.close();
+        fileInputStream.close();
     }
 }
