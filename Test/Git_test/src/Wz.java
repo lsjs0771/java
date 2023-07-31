a256c009e90bf7d49a0cfa40639ac019727d2377
lsjs0771 <0771wangzhong@gmail.com>
2023-07-31 09:27:23 +0800

    wzpc

--- Test/Git_test/src/Wz.java
+++ Test/Git_test/src/Wz.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 public class Wz{
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("ok");
+        System.out.println("hello nanning");
         String s="/home/wjs/12.png";
         File file=new File(s);
         String name=file.getName();
